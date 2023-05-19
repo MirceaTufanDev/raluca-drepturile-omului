7261196afec64f72ca518ae0a1cb73041d6219ce
mircea.tufan <mircea.tufan@principal33.com>
2023-05-19 13:14:59 +0300

    first

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Realizat de Raluca - Mariana ANDREIU (STEFAN)';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Next.js Blog Theme';
